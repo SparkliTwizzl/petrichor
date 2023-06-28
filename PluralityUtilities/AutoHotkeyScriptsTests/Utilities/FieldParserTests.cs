2f919dd18c85393a359c5ad430de460a24dee498
Petra Sparrow (xey/xem) | SparkliTwizzl <github@sparklitwizzl.com>
2023-06-28 00:22:35 -0600

    add test class for TokenParser and FieldParser

---
+++ PluralityUtilities/AutoHotkeyScriptsTests/Utilities/FieldParserTests.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace PluralityUtilities.AutoHotkeyScripts.Utilities.Tests
+{
+	[ TestClass ]
+	public class FieldParserTests
+	{
+	}
+}
---
+++ PluralityUtilities/CommonTests/Utilities/TokenParserTests.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace PluralityUtilities.AutoHotkeyScripts.Utilities.Tests
+{
+	[ TestClass ]
+	public class TokenParserTests
+	{
+		[ TestMethod ]
+		public void ParseTokenTest_Success()
+		{
+			Assert.Fail();
+		}
+	}
+}
