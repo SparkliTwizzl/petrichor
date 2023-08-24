aa21a4984caa36bdbbce77842751707a9eb6bff2
Alix Barbosa (it/its) | SparkliTwizzl <github@sparklitwizzl.com>
2023-08-24 01:45:06 -0600

    start implementing EntryDataParser

--- PluralityUtilities/AutoHotkeyScripts/Utilities/EntryDataParser.cs
+++ PluralityUtilities/AutoHotkeyScripts/Utilities/EntryDataParser.cs
@@ -1,13 +1,64 @@
 ﻿using PluralityUtilities.Common.Containers;
-
+using PluralityUtilities.Common.Utilities;
+using PluralityUtilities.Logging;
 
 namespace PluralityUtilities.AutoHotkeyScripts.Utilities
 {
-	public static class EntryDataParser
+	public class EntryDataParser
 	{
-		public static Token[] ParseEntryDataRegion( Token regionToken )
+		public const string RegionName = "entries";
+		public string[] AllowedFieldNames { get; set; }
+		public Dictionary<string, string[]> FieldDictionary { get; set; }
+
+
+		public EntryDataParser( Dictionary<string, string[]> fieldDictionary )
 		{
+			FieldDictionary = fieldDictionary;
+			var allowedFieldNameList = new List<string>();
+			foreach ( var item in fieldDictionary )
+			{
+				allowedFieldNameList.Add( item.Key );
+			}
+			AllowedFieldNames = allowedFieldNameList.ToArray();
+		}
+
+
+		public Token[] ParseEntryDataRegion( Token regionToken )
+		{
+			var tokenList = TokenDataParser.FlattenTokenTree( regionToken );
+			RegionDataValidator.ValidateBasicRegionData( tokenList, RegionName, AllowedFieldNames );
+			ValidateFieldStructure( tokenList );
 			throw new NotImplementedException();
+		}
+
+
+		private void ValidateFieldStructure( Token[] tokens )
+		{
+			for ( var i = 1; i < tokens.Length; ++i )
+			{
+				var token = tokens[ i ];
+				var allowedChildFieldNames = FieldDictionary[ token.Name ];
+				foreach ( var childToken in token.Body )
+				{
+					var isChildNameAnAllowedField = false;
+
+					foreach ( var allowedFieldName in allowedChildFieldNames )
+					{
+						if ( string.Compare( childToken.Name, allowedFieldName ) == 0 )
+						{
+							isChildNameAnAllowedField = true;
+							break;
+						}
+					}
+
+					if ( !isChildNameAnAllowedField )
+					{
+						var e = new InvalidDataException( $"a token with name '{ token.Name }' in region '{ RegionName }' contained a subtoken with a name that did not match field definitions [[ { childToken.Name } ]]" );
+						Log.Exception( e );
+						throw e;
+					}
+				}
+			}
 		}
 	}
 }
--- PluralityUtilities/AutoHotkeyScriptsTests/Utilities/EntryDataParserTests.cs
+++ PluralityUtilities/AutoHotkeyScriptsTests/Utilities/EntryDataParserTests.cs
@@ -1,16 +1,56 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PluralityUtilities.Common.Containers;
 using PluralityUtilities.TestCommon.Utilities;
 
 
 namespace PluralityUtilities.AutoHotkeyScripts.Utilities.Tests
 {
-	[ TestClass ]
+	[TestClass]
 	public class EntryDataParserTests
 	{
 		[ TestInitialize ]
 		public void Setup()
 		{
 			TestUtilities.InitializeLoggingForTests();
+		}
+
+
+		private static IEnumerable<object[]> Data_ParseEntryDataRegion_Success()
+		{
+			yield return new TestData.DataContainer_ParseEntryDataRegion_Success
+			{
+			}.ToObjectArray();
+		}
+
+		[ TestMethod ]
+		[ DynamicData(
+			nameof( Data_ParseEntryDataRegion_Success ),
+			DynamicDataSourceType.Method ) ]
+		public void Test_ParseEntryDataRegion_Success(
+			Token[] expected, Token input )
+		{
+			Assert.Fail();
+		}
+
+
+		private static class TestData
+		{
+			public static Token RegionToken { get; set; } = new();
+			public static Token[] ParsedEntryData { get; set; } =
+			{
+			};
+
+
+			public struct DataContainer_ParseEntryDataRegion_Success
+			{
+				public Token[] Expected { get; set; }
+				public Token Input { get; set; }
+
+				public object[] ToObjectArray()
+				{
+					return new object[] { Expected, Input };
+				}
+			}
 		}
 	}
 }
