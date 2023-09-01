abcac0c4fe156004cbea945ba51c1465e45a6a8b
Colleen Foster (she/her) | SparkliTwizzl <github@sparklitwizzl.com>
2023-09-01 16:59:05 -0600

    create test class for MacroGenerator

---
+++ PluralityUtilities/AutoHotkeyScriptsTests/Utilities/MacroGeneratorTests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PluralityUtilities.Common.Containers;
+using PluralityUtilities.TestCommon.Utilities;
+
+
+namespace PluralityUtilities.AutoHotkeyScripts.Utilities.Tests
+{
+	[ TestClass ]
+	public class MacroGeneratorTests
+	{
+		[ TestInitialize ]
+		public void Setup()
+		{
+			TestUtilities.InitializeLoggingForTests();
+		}
+
+
+		private static IEnumerable<object[]> Data_GenerateMacros_Success()
+		{
+			yield return new TestData.DataContainer_GenerateMacros_Success
+			{
+			}.ToObjectArray();
+		}
+
+		[TestMethod ]
+		[ DynamicData(
+			nameof( Data_GenerateMacros_Success ),
+			DynamicDataSourceType.Method ) ]
+		public void Test_GenerateMacros_Success()
+		{
+			Assert.Fail();
+		}
+
+
+		private static class TestData
+		{
+			public struct DataContainer_GenerateMacros_Success
+			{
+				public string[] Expected { get; set; }
+				public Token[] Input { get; set; }
+
+				public object[] ToObjectArray() => new object[]
+				{
+						Expected,
+						Input,
+				};
+			}
+		}
+	}
+}
